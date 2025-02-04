fecdbfc27b56c058c32c82659966e5d529f92347
Kotohara-Senichi <166996675+Kotohara-Senichi@users.noreply.github.com>
2025-02-04 22:50:12 +0900

    最初のコミットです。

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("確認を行うドメインを入力してください");
+
+        String CheckDomain = scanner.nextLine();
+        System.out.println(CheckDomain +"の設定を検索します。");
+    }
+}
