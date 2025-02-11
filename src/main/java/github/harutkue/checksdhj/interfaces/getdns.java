719d389887472e042ece6f5b3d8176fea386990d
kotohara-Senichi <166996675+Ooba-haruki@users.noreply.github.com>
2025-02-11 16:07:48 +0000

    やれることはやりました。cfは動作未確認、作成中です。

--- src/main/java/github/harutkue/checksdhj/interfaces/getdns.java
+++ src/main/java/github/harutkue/checksdhj/interfaces/getdns.java
@@ -12,6 +12,9 @@ import org.xbill.DNS.Record;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import github.harutkue.checksdhj.interfaces.Providerlist;
+
 //Aレコードを取得するプログラム(CheckMethodsで用いる)
 public class getdns {
     public static List<String> getArecords(String Domain){
