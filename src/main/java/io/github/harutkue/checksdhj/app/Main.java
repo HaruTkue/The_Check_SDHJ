66fcde6aeac43b6ef4f62608c427ed7e74a05a21
机上-Haruca <166996675+HaruTkue@users.noreply.github.com>
2025-03-10 06:18:42 +0000

    修正

--- src/main/java/io/github/harutkue/checksdhj/app/Main.java
+++ src/main/java/io/github/harutkue/checksdhj/app/Main.java
@@ -10,16 +10,25 @@ import io.github.harutkue.checksdhj.networks.network;
 public class Main {
     public static void main(String[] args) {
         //作成
+        if(args.length ==0){
+            System.out.println("適切な引数を指定できていません。");
+        }
         network get_Connection = new network();
+        //スキャナ類の処理はいったん使用しない。
+        /*
         Scanner scanner = new Scanner(System.in);
         System.out.println("確認を行うドメインを入力してください");
 
         String CheckDomain = scanner.nextLine();
         System.out.println(CheckDomain + "の設定を検索します。");
         scanner.close();
-
-        //cnameを取得する
-       get_Connection.Get_CNAME(CheckDomain);
+        */
+        for(String target:args){
+            System.out.println("指定されたサブドメイン:" + target);
+            //cnameを取得する
+            String CheckDomain = target;
+            get_Connection.Get_CNAME(CheckDomain);
+        }
     }
 
 }
